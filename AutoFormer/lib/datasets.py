0f8b9217a0129bc407c0027f1af3f426624137a5
Eric Zheng <ezhe473@foscsmlprd03.its.auckland.ac.nz>
2025-10-19 17:16:56 +1300

    feat: updated `datasets.py` with the ATRW dataloader

--- AutoFormer/lib/datasets.py
+++ AutoFormer/lib/datasets.py
@@ -1,3 +1,4 @@
+import glob
 import os
 import cv2
 import json
@@ -6,24 +7,30 @@ import scipy
 import scipy.io as sio
 from skimage import io
 
+from torch.utils.data import Dataset
 from torchvision import datasets, transforms
 from torchvision.datasets.folder import ImageFolder, default_loader
 
 from timm.data.constants import IMAGENET_DEFAULT_MEAN, IMAGENET_DEFAULT_STD
 from timm.data import create_transform
 
-class Flowers(ImageFolder):
+# elif args.data_set == 'FLOWERS':
+#     dataset = Flowers(args.data_path, train=is_train, transform=transform)
+#     nb_classes = 102
+
+
+class Flowers(ImageFolder):  # inherits `ImageFolder` from `torchvision.datasetsA`
     def __init__(self, root, train=True, transform=None, **kwargs):
-        self.dataset_root = root
-        self.loader = default_loader
+        self.dataset_root = root                           # root path to dataset
+        self.loader = default_loader                       # whether to load "training data" or "test data"
         self.target_transform = None
-        self.transform = transform
-        label_path = os.path.join(root, 'imagelabels.mat')
-        split_path = os.path.join(root, 'setid.mat')
+        self.transform = transform                         # image transformation pipeline (e.g. resize, crop, rotation)
+        label_path = os.path.join(root, 'imagelabels.mat')  # Matlab files containing labels for all images
+        split_path = os.path.join(root, 'setid.mat')       # Matlab files containing indices of images split into train/val/test sets
 
         print('Dataset Flowers is trained with resolution 224!')
 
-        # labels
+        # set up `img_to_label` such that `img_to_label[20] = "image_0020.jpg"`
         labels = sio.loadmat(label_path)['labels'][0]
         self.img_to_label = dict()
         for i in range(len(labels)):
@@ -31,8 +38,8 @@ class Flowers(ImageFolder):
 
         splits = sio.loadmat(split_path)
         self.trnid, self.valid, self.tstid = sorted(splits['trnid'][0].tolist()), \
-                                             sorted(splits['valid'][0].tolist()), \
-                                             sorted(splits['tstid'][0].tolist())
+            sorted(splits['valid'][0].tolist()), \
+            sorted(splits['tstid'][0].tolist())
         if train:
             self.imgs = self.trnid + self.valid
         else:
@@ -40,7 +47,8 @@ class Flowers(ImageFolder):
 
         self.samples = []
         for item in self.imgs:
-            self.samples.append((os.path.join(root, 'jpg', "image_{:05d}.jpg".format(item)), self.img_to_label[item-1]-1))
+            self.samples.append((os.path.join(root, 'jpg', "image_{:05d}.jpg".format(item)), self.img_to_label[item - 1] - 1))
+
 
 class Cars196(ImageFolder, datasets.CIFAR10):
     base_folder_devkit = 'devkit'
@@ -56,7 +64,7 @@ class Cars196(ImageFolder, datasets.CIFAR10):
         ['00002.jpg', '4b9e5efcc3612378ec63a22f618b5028']
     ]
     test_list = []
-    num_training_classes = 98 # 196/2
+    num_training_classes = 98  # 196/2
 
     def __init__(self, root, train=False, transform=None, target_transform=None, **kwargs):
         self.root = root
@@ -71,18 +79,19 @@ class Cars196(ImageFolder, datasets.CIFAR10):
 
         if train:
             labels = \
-            sio.loadmat(os.path.join(self.root, self.base_folder_devkit, self.filename_trainanno))['annotations'][0]
+                sio.loadmat(os.path.join(self.root, self.base_folder_devkit, self.filename_trainanno))['annotations'][0]
             for item in labels:
                 img_name = item[-1].tolist()[0]
                 label = int(item[4]) - 1
                 self.samples.append((os.path.join(self.root, self.base_folder_trainims, img_name), label))
         else:
             labels = \
-            sio.loadmat(os.path.join(self.root, 'cars_test_annos_withlabels.mat'))['annotations'][0]
+                sio.loadmat(os.path.join(self.root, 'cars_test_annos_withlabels.mat'))['annotations'][0]
             for item in labels:
                 img_name = item[-1].tolist()[0]
                 label = int(item[-2]) - 1
                 self.samples.append((os.path.join(self.root, self.base_folder_testims, img_name), label))
+
 
 class Pets(ImageFolder):
     def __init__(self, root, train=True, transform=None, target_transform=None, **kwargs):
@@ -99,13 +108,14 @@ class Pets(ImageFolder):
                 for line in f:
                     img_name = line.split(' ')[0]
                     label = int(line.split(' ')[1])
-                    self.samples.append((os.path.join(root, 'images', "{}.jpg".format(img_name)), label-1))
+                    self.samples.append((os.path.join(root, 'images', "{}.jpg".format(img_name)), label - 1))
         else:
             with open(test_list_path, 'r') as f:
                 for line in f:
                     img_name = line.split(' ')[0]
                     label = int(line.split(' ')[1])
-                    self.samples.append((os.path.join(root, 'images', "{}.jpg".format(img_name)), label-1))
+                    self.samples.append((os.path.join(root, 'images', "{}.jpg".format(img_name)), label - 1))
+
 
 class INatDataset(ImageFolder):
     def __init__(self, root, train=True, year=2018, transform=None, target_transform=None,
@@ -149,6 +159,7 @@ class INatDataset(ImageFolder):
 
     # __getitem__ and __len__ inherited from ImageFolder
 
+
 def build_dataset(is_train, args, folder_name=None):
     transform = build_transform(is_train, args)
 
@@ -183,8 +194,14 @@ def build_dataset(is_train, args, folder_name=None):
         dataset = INatDataset(args.data_path, train=is_train, year=2019,
                               category=args.inat_category, transform=transform)
         nb_classes = dataset.nb_classes
+    elif args.data_set == 'ATRW':
+        split = 'train' if is_train else folder_name  # folder_name can be 'gallery' or 'query'
+        transform = build_reid_transform(is_train=(split == 'train'), input_size=args.input_size)
+        dataset = ATRWReID(args.data_path, split=split, transform=transform)
+        nb_classes = dataset.nb_classes
 
     return dataset, nb_classes
+
 
 def build_transform(is_train, args):
     resize_im = args.input_size > 32
@@ -217,4 +234,70 @@ def build_transform(is_train, args):
 
     t.append(transforms.ToTensor())
     t.append(transforms.Normalize(IMAGENET_DEFAULT_MEAN, IMAGENET_DEFAULT_STD))
+    return transforms.Compose(t)
+
+
+class ATRW(Dataset):
+    def __init__(self, root, split='train', transform=None):
+        self.root = root
+        self.split = split
+        self.transform = transform
+
+        # Assume all images are in root/*.jpg
+        pattern = os.path.join(root, '*.jpg')
+        self.img_paths = sorted(glob.glob(pattern))
+
+        if len(self.img_paths) == 0:
+            raise RuntimeError(f'No images found in {pattern}')
+
+        # Extract all PIDs from filenames
+        pids = []
+        for img_path in self.img_paths:
+            fname = os.path.basename(img_path)
+            pid = int(fname.split('_')[0])  # e.g., 3_-1_0_005142.jpg → pid=3
+            pids.append(pid)
+
+        # Build a PID → label mapping
+        unique_pids = sorted(set(pids))
+        self.pid2label = {pid: idx for idx, pid in enumerate(unique_pids)}
+        self.num_pids = len(unique_pids)
+
+        # Build final samples list with mapped labels
+        self.samples = []
+        for img_path in self.img_paths:
+            fname = os.path.basename(img_path)
+            pid = int(fname.split('_')[0])
+            label = self.pid2label[pid]
+            self.samples.append((img_path, label))
+
+    def __len__(self):
+        return len(self.samples)
+
+    def __getitem__(self, idx):
+        path, label = self.samples[idx]
+        img = Image.open(path).convert('RGB')
+        if self.transform:
+            img = self.transform(img)
+        return img, label, path
+
+
+def build_reid_transform(is_train=True, input_size=224):
+    resize_size = input_size
+
+    if is_train:
+        t = [
+            transforms.Resize((resize_size, resize_size)),
+            transforms.RandomHorizontalFlip(p=0.5),
+            transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1),
+            transforms.ToTensor(),
+            transforms.Normalize(IMAGENET_DEFAULT_MEAN, IMAGENET_DEFAULT_STD),
+            transforms.RandomErasing(p=0.5, scale=(0.02, 0.2))
+        ]
+    else:
+        t = [
+            transforms.Resize((resize_size, resize_size)),
+            transforms.ToTensor(),
+            transforms.Normalize(IMAGENET_DEFAULT_MEAN, IMAGENET_DEFAULT_STD)
+        ]
+
     return transforms.Compose(t)
